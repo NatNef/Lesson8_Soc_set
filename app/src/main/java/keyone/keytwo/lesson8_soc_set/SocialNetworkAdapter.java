33da7cd5697c8c16e5244d9b0cb46d37625be26f
Natalia <nef.n@list.ru>
2021-08-24 12:54:29 +0300

    урок 8

--- app/src/main/java/keyone/keytwo/lesson8_soc_set/SocialNetworkAdapter.java
+++ app/src/main/java/keyone/keytwo/lesson8_soc_set/SocialNetworkAdapter.java
@@ -1,12 +1,26 @@
 package keyone.keytwo.lesson8_soc_set;
 
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 // класс для обслуживания RecyclerView
-public class SocialNetworkAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class SocialNetworkAdapter extends RecyclerView.Adapter<SocialNetworkAdapter.MyViewHolder> {
+
+    //xml файл -inflate- теперь он java - разбиваем этот объект на
+    // оболочку MyViewHolder что-бы обращаться к его элементам  textView и imageView
+    // достаем элементы public MyViewHolder и далее задаются значения onBindViewHolder
+
+
+
+
 
     // задаем данные
     private String[] dadaSource;
@@ -21,20 +35,54 @@ public class SocialNetworkAdapter extends RecyclerView.Adapter<RecyclerView.View
 
 
 
-    // создаются элементы
+    // создаются элементы мы создаем для него оболочку MyViewHolder
+    // делаем доступ к его полям TextView
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent,false);
+
+//        MyViewHolder holder = new MyViewHolder(view);
+//        holder.imageView
+        return new MyViewHolder(view);
     }
 
+
+    //связивает данные View со значением
+    // прокрутился список и создается след элемент который уехал за экран
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.textView.setText(dadaSource[position]);
+//        holder.imageView.setText(dadaSource[position]);
 
     }
 
-    //вернуть размер
+    //вернуть размер. сколько прокручивается
     @Override
     public int getItemCount() {
         return dadaSource.length;
     }
+
+    // внутри адаптера класс, будет предатсавлением в
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        // элементы
+        private TextView textView;
+        private ImageView imageView;
+
+//        public TextView getTextView() {
+//            return textView;
+//        }
+
+//        public void setTextView(TextView textView) {
+//            this.textView = textView;
+//        }
+
+ //приходят элементы и мы достаем
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+            itemView = itemView.findViewById(R.id.imageView);
+
+        }
+    }
+
 }
--- app/src/main/java/keyone/keytwo/lesson8_soc_set/SocialNetworkFragment.java
+++ app/src/main/java/keyone/keytwo/lesson8_soc_set/SocialNetworkFragment.java
@@ -1,6 +1,34 @@
 package keyone.keytwo.lesson8_soc_set;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SocialNetworkFragment extends Fragment {
+
+    public static SocialNetworkFragment newInstance()
+    {return new SocialNetworkFragment();
+
+    }
+
+    @Nullable
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_social_network, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());  // отвечает за размещение элементов в RecyclerViewБ будут вертикально вниз
+        recyclerView.setLayoutManager(LayoutManager);
+
+        String[] data = getResources().getStringArray(R.array.test_title);
+        SocialNetworkAdapter socialNetworkAdapter = new SocialNetworkAdapter(data);
+
+        return view;
+    }
 }
